b1abf115ced3c438651600761dd592a6eae7e2c9
Calum Russell <calum.mj.russell@gmail.com>
2020-08-28 18:31:55 +0100

    add whitelisting for code tags for markdown

--- retirement/settings.py
+++ retirement/settings.py
@@ -140,7 +140,8 @@ MARKDOWNIFY_WHITELIST_TAGS = {
  'a', 'p', 'em',
  'h1', 'h2', 'h3','h4', 'h5', 'h6', 'h7',
  'ul', 'li', 'span', 'strong', 'ol', 'img',
- 'table', 'thead', 'tbody', 'tr', 'th', 'td'
+ 'table', 'thead', 'tbody', 'tr', 'th', 'td',
+ 'code'
 }
 
 MARKDOWNIFY_WHITELIST_ATTRS = [
