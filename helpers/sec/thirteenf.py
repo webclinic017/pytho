7aea9bcaf9bdf3792384916468ae6979011e3889
Calum Russell <calum.mj.russell@gmail.com>
2021-11-12 05:26:34 +0000

    added thirteenffetcher

--- helpers/sec/thirteenf.py
+++ helpers/sec/thirteenf.py
@@ -1,6 +1,9 @@
 from typing import TypedDict, List
 from bs4 import BeautifulSoup as bs
 from bs4.element import ResultSet
+import requests
+
+from api.models import SecFilingPaths
 
 
 class Position(TypedDict):
@@ -9,6 +12,19 @@ class Position(TypedDict):
     value: int
     shares: int
     shares_type: str
+
+class ThirteenFFetcher:
+    def _fetch(self):
+        path = self.filing_path_obj.path
+        url = f'https://sec.report/Document/{path}/form13fInfoTable.xml'
+        client = requests.Session()
+        r = client.get(url, headers={"User-Agent": "Pytho"})
+        self.data = r.text
+        return
+
+    def __init__(self, filing_path_obj: SecFilingPaths):
+        self.filing_path_obj = filing_path_obj
+        self._fetch()
 
 
 class ThirteenFParser:
@@ -29,7 +45,6 @@ class ThirteenFParser:
         self.xml = bs(xml, "lxml")
         self.info_tables: ResultSet = self.xml.find_all("infotable")
         self._parse()
-        return
 
     def get_positions(self) -> List[Position]:
         return self.positions
