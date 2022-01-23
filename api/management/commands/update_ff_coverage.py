4c7fda5f196678726ca7779376a4f17cef936780
calum Russell <calum.mj.russell@gmail.com>
2022-01-23 03:51:51 +0000

    added task to update factor coverages

---
+++ api/management/commands/update_ff_coverage.py
@@ -0,0 +1,25 @@
+from api.models import FactorReturns, Coverage
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        temp = set()
+        for i in FactorReturns.objects.all():
+            to_dict = i.__dict__
+            name = to_dict['name']
+            factor = to_dict['factor']
+            combo = name + "-" + factor
+            temp.add(combo)
+        temp1 = []
+        for t in temp:
+            c = Coverage(
+                country_name="international",
+                name=t,
+                issuer=None,
+                currency=None,
+                ticker=None,
+                security_type="factor"
+            )
+            temp1.append(c)
+        Coverage.objects.bulk_create(temp1)
+        return
