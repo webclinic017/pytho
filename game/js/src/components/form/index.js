1f6f6f69adef6af376da0b6e9f1c16cfe029ca33
Calum <calum.mj.russell@gmail.com>
2022-04-21 17:04:39 +0000

    significant fixes to standard form, removing dependency on pure

--- game/js/src/components/form/index.js
+++ game/js/src/components/form/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import {
   getCssVar,
@@ -11,12 +12,6 @@ export const FormSelect = (props) => (
     {
       props.options.map((o, i) => (
         <option
-          style={
-            {
-              background: 'var(--default-background-color)',
-              color: 'var(--default-text-color)',
-            }
-          }
           value={ o }
           key={ i }>
           {o}
@@ -36,23 +31,12 @@ FormSelect.propTypes = {
 
 export const FormInput = (props) => (
   <input
-    { ...props }
-    style={
-      {
-        fontSize: '0.8rem',
-      }
-    } />
+    { ...props } />
 );
 
 export const FormLabel = (props) => {
   return (
     <label
-      style={
-        {
-          fontSize: '0.9rem',
-          color: getCssVar('--alt-text-color'),
-        }
-      }
       { ...props }>
       {props.children}
     </label>
@@ -66,15 +50,67 @@ FormLabel.propTypes = {
   ]),
 };
 
+const StyledForm = styled.form`
+  input[type="text"] {
+    background-color: transparent;
+    box-shadow: none;
+    border: 1px solid var(--alt-background-color);
+    color: var(--default-text-color);
+    padding: 0.5rem 0.5rem;
+    border-radius: 0px;
+    font-size: 0.9rem;
+    width: 100%;
+    height: 2rem;
+
+    &:placeholder-shown {
+      font-style: italic;
+    }
+  }
+
+  fieldset {
+    border: none;
+    
+    label {
+      margin: 0.5rem 0;
+    }
+  }
+
+  select {
+    height: 2rem;
+    padding: 0.25rem 0.5rem;
+    margin: 0.5rem 0;
+    background-color: transparent;
+    box-shadow: none;
+    border: 1px solid var(--alt-background-color);
+    color: var(--alt-text-color);
+    font-size: 0.9rem;
+
+    &:first-child {
+      margin-top: 0;
+      margin-bottom: 0.5rem;
+    }
+
+    option {
+      color: var(--alt-text-color);
+      background-color: var(--default-background-color);
+    }
+
+    option:first-child {
+      color: var(--alt-text-color);
+      background: var(--default-background-color);
+    }
+  }
+`;
+
+
 export const FormWrapper = (props) => {
   return (
-    <form
-      { ...props }
-      className="pure-form pure-form-stacked">
+    <StyledForm
+      { ...props }>
       <fieldset>
         {props.children}
       </fieldset>
-    </form>
+    </StyledForm>
   );
 };
 
